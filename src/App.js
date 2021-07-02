af23d9f3aeafe984d22a34243833c48ce7b04a73
Rogério Venâncio <50733125+RogVenancio@users.noreply.github.com>
2021-07-02 12:58:34 -0300

    Front-end para Deployment

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Lista from "./components/lista";
+// import "./styles/reset.css";
+
+function App() {
+  return (
+    <div>
+      <div>
+        <Lista/>
+      </div>
+    </div>
+  );
+}
+
+export default App;
