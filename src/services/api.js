4f1abf1bba21cdac0b67a41b5268d60c87590de1
RogVenancio <50733125+RogVenancio@users.noreply.github.com>
2021-07-03 03:29:05 -0300

    Correção de rota

--- src/services/api.js
+++ src/services/api.js
@@ -1,6 +1,5 @@
 import Axios from "axios";
 
 export default Axios.create({
-  // baseURL: process.env.REACT_APP_API_URL
-  baseURL: "http://localhost:3333/"
+  baseURL: process.env.REACT_APP_API_URL,
 });
